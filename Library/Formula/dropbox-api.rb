667729ef8b4657735c73dc5f6c358e9320ff1bdb
s-aska <s.aska.org@gmail.com>
2012-10-11 21:29:16 +0900

    added depends_on

--- Library/Formula/dropbox-api.rb
+++ Library/Formula/dropbox-api.rb
@@ -4,16 +4,14 @@ class DropboxApi <Formula
   head 'git://github.com/s-aska/dropbox-api-command.git'
   homepage 'https://github.com/s-aska/dropbox-api-command'
 
-  depends_on 'cpanm'
+  depends_on 'JSON' => :perl
+  depends_on 'Path::Class' => :perl
+  depends_on 'WebService::Dropbox' => :perl
+  depends_on 'DateTime::Format::Strptime' => :perl
+  depends_on 'Encode::Locale' => :perl
+  depends_on 'Encode::UTF8Mac' => :perl
 
   def install
     bin.install 'dropbox-api'
-  end
-
-  def caveats; <<-EOS.undent
-      ========== you must install the Perl Module ==========
-        $ cpanm --sudo JSON Path::Class WebService::Dropbox DateTime::Format::Strptime Encode::Locale Encode::UTF8Mac
-      ======================================================
-    EOS
   end
 end
